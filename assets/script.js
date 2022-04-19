41daeb95b16b1bd6c464a691a492f672d57fbcf2
rodolfopoly <rodolfopoly@gmail.com>
2022-04-18 20:45:05 -0400

    2 apis

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
 var repoList = document.querySelector('ul');
-var fetchButton = document.getElementById('fetch-button');
 
-function getApi() {
+function getApibored() {
   // Replace `octocat` with anyone else's GitHub username
   var requestUrl = 'https://foodish-api.herokuapp.com/images/api/#';
 
@@ -14,4 +13,19 @@ function getApi() {
     });
 }
 
-fetchButton.addEventListener('click', getApi);
+fetchButton.addEventListener('click', getApibored);
+
+function getApihungry() {
+  // Replace `octocat` with anyone else's GitHub username
+  var requestUrl = 'https://foodish-api.herokuapp.com/images/api/#';
+
+  fetch(requestUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data)
+    });
+}
+
+fetchButton.addEventListener('click', getApihungry);
