81ca8a4fd07190e8bb86c77a3b7b3ac395f663e2
lissygonz <99147859+lissygonz@users.noreply.github.com>
2022-04-20 19:40:04 -0400

    added comments and local storage 1st line

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-
+//function to get activity
 function getBoredApi() {
   var requestUrl = 'http://www.boredapi.com/api/activity/';
   fetch(requestUrl)
@@ -10,7 +10,7 @@ function getBoredApi() {
        var h3 = $("#h3").text(data.activity);
        $('#apiContainer').append(h3);
     });
-}
+}//function to get food image
 function getFoodApi() {
   var requestUrl = 'https://foodish-api.herokuapp.com/api/';
 
@@ -25,6 +25,10 @@ function getFoodApi() {
     });
 }
 
+//event listener for button to get activity and food image 
 $("#foodActivityButton").on("click", getBoredApi);
 $("#foodActivityButton").on("click", getFoodApi);
 
+
+//set submission for local storage
+localStorage.setItem("response", JSON.stringify(response));
