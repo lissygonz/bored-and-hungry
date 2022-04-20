af2d5d54f123dece1b550a79207cb6afe729959f
lissygonz <99147859+lissygonz@users.noreply.github.com>
2022-04-20 19:47:11 -0400

    added local storage

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,38 @@
-var repoList = document.querySelector('ul');
-
-function getApibored() {
-  // Replace `octocat` with anyone else's GitHub username
-  var requestUrl = 'https://foodish-api.herokuapp.com/images/api/#';
-
+//function to get activity
+function getBoredApi() {
+  var requestUrl = 'http://www.boredapi.com/api/activity/';
   fetch(requestUrl)
-    .then(function(response) {
+    .then(function(response){
       return response.json();
     })
     .then(function(data) {
-      console.log(data)
+       console.log(data.activity);
+       var h3 = $("#h3").text(data.activity);
+       $('#apiContainer').append(h3);
     });
-}
-
-fetchButton.addEventListener('click', getApibored);
-
-function getApihungry() {
-  // Replace `octocat` with anyone else's GitHub username
-  var requestUrl = 'https://foodish-api.herokuapp.com/images/api/#';
+}//function to get food image
+function getFoodApi() {
+  var requestUrl = 'https://foodish-api.herokuapp.com/api/';
 
   fetch(requestUrl)
-    .then(function(response) {
+    .then(function(response){
       return response.json();
     })
     .then(function(data) {
-      console.log(data)
+       console.log(data.image);
+       var img = $("#image").attr("src", data.image)
+       $('#apiContainer').append(img);
     });
 }
 
-fetchButton.addEventListener('click', getApihungry);
+//event listener for button to get activity and food image 
+$("#foodActivityButton").on("click", getBoredApi);
+$("#foodActivityButton").on("click", getFoodApi);
+
+
+
+//set submission for local storage
+localStorage.setItem("response", JSON.stringify(response));
+
+const result = localStorage.getItem('response')
+
