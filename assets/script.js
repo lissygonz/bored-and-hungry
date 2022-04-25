5fe7f09e41047e67cd41ea9f64ae39699c5425bc
rodolfopoly <rodolfopoly@gmail.com>
2022-04-25 18:42:00 -0400

    script changes

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,6 @@ var historyTest = [];
 const apiKey = "9319583f09msh1867038f13b6972p19a9c0jsnb8d29d2b9b68"
 
 
-
 //function to get activity
 function getBoredApi() {
   var requestUrl = 'http://www.boredapi.com/api/activity/';
@@ -15,12 +14,13 @@ function getBoredApi() {
     .then(function (data) {
       activity = data.activity;
       console.log(data.activity);
-      var h3 = $("#h3").text(data.activity);
-      $('#apiContainer').append(h3);
-
-
+      var activity = data.activity
+      var h3 = $("<h3>").text(activity);
+      $('#subContainer').append(h3);
     });
-}//function to get food image
+}
+
+//function to get food image
 function getFoodApi() {
   const options = {
     method: 'GET',
@@ -29,11 +29,22 @@ function getFoodApi() {
       'X-RapidAPI-Key': '9319583f09msh1867038f13b6972p19a9c0jsnb8d29d2b9b68'
     }
   };
-  
-  fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/479101/information', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+
+  fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random', options)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      $('#subContainer').empty()
+      var icon = data.recipes[0].image
+      var image = $("<img>").attr("src", icon);
+      var recipe = data.recipe[0].instructions
+      var precipe = $("<p>").text(recipe)
+      $('#subContainer').append(image);
+      $('#subContainer').append(precipe);
+
+    });
 }
 function saveButton() {
   var user = {
@@ -44,27 +55,11 @@ function saveButton() {
   historyTest.push(user);
   localStorage.setItem("history", JSON.stringify(historyTest));
   console.log(historyTest);
-
-
-
-
 };
-
-
-
-
-
-
 
 //event listener for button to get activity and food image 
 $("#foodActivityButton").on("click", getBoredApi);
 $("#foodActivityButton").on("click", getFoodApi);
 $("#storage").on("click", saveButton);
-
-
-
-
-//set submission for local storage
-localStorage.setItem("response", JSON.stringify(response));
 
 
