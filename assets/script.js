dc1f4549d19a7cf7a946a47909eda96ff600ac45
rodolfopoly <rodolfopoly@gmail.com>
2022-04-27 12:53:15 -0400

    add to recent 1

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,14 @@
-var image = 0;
-var activity = 0;
+var title ;
+var icon ;
+var recipe ;
+var activity ;
 var historyTest = [];
+var result = false;
 const apiKey = "9319583f09msh1867038f13b6972p19a9c0jsnb8d29d2b9b68"
 
 function start(e) {
   e.preventDefault();
+  result = true
   var cookTime = $('#cook-time').val();
   console.log(cookTime);
   var howMany = $('#how-many').val();
@@ -26,7 +30,6 @@ function getBoredApi(howMany) {
       $('#activities-item-container').empty()
       activity = data.activity;
       console.log(data.activity);
-      var activity = data.activity
       var h3 = $("<h3>").text(activity);
       $('#activities-item-container').append(h3);
     });
@@ -49,11 +52,11 @@ function getFoodApi(cookTimeF) {
     .then(function (data) {
       function getRecipe() {
       console.log(data);
-      var title = data.recipes[0].title
+      title = data.recipes[0].title
       var h2 = $("<h2>").text(title).css({"font-size": "40px"});
-      var icon = data.recipes[0].image
+      icon = data.recipes[0].image
       var image = $("<img>").attr("src", icon).css({"width": "400px", "border-radius": "20px"});
-      var recipe = data.recipes[0].sourceUrl
+      recipe = data.recipes[0].sourceUrl
       var linkRecipe = $("<a>").text("Link to recipe").attr("href", recipe).css({"padding": "5px 5px 5px 5px", "background-color": "rgb(199, 40, 40)", "text-decoration": "none", "color": "white",});
       $('#img-col').append(image);
       $('#text-col').append(h2);
@@ -83,12 +86,33 @@ function getFoodApi(cookTimeF) {
 function saveButton() {
   var user = {
     activity: activity,
-    image: image
+    image: icon,
+    title: title,
+    recipe: recipe
   };
+  $('#img-col-r1').empty()
+  $('#text-col-r1').empty()
+  $('#link-col-r1').empty()
   // set new submission to local storage
-  historyTest.push(user);
-  localStorage.setItem("history", JSON.stringify(historyTest));
-  console.log(historyTest);
+
+  if (result == true)
+   {
+    historyTest.push(user);
+    localStorage.setItem("historyTest", JSON.stringify(historyTest));
+       //var historyTmp = localStorage.getItem("historyTest");
+      console.log(historyTest[historyTest.length-1].activity);
+       var h2 = $("<h2>").text(historyTest[0].activity).css({"font-size": "10px"});
+       var image = $("<img>").attr("src", icon).css({"width": "40px", "border-radius": "20px"});
+       var linkRecipe = $("<a>").text("Link to recipe").attr("href", recipe).css({"padding": "5px 5px 5px 5px", "background-color": "rgb(199, 40, 40)", "text-decoration": "none", "color": "white",});
+             $('#img-col-r1').append(image);
+             $('#text-col-r1').append(h2);
+             $('#text-col-r1').append(linkRecipe);
+       
+       
+   }
+   else{
+
+   }
 };
 
 //event listener for button to get activity and food image 
